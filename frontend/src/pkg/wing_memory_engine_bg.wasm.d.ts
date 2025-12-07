50fdaf77482620f89bfbf8f853189697fb297255
Bruno <bruno@awdd.com.br>
2025-12-07 15:52:53 -0300

    Add TypeScript definitions for WebAssembly module 'wing_memory_engine'
    - Created a new TypeScript declaration file for the WebAssembly module.
    - Defined exports including memory, main_js, and various functions for model loading, token ingestion, querying, and serialization.
    - Included memory management functions for allocation and deallocation.

---
+++ frontend/src/pkg/wing_memory_engine_bg.wasm.d.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main_js: () => void;
+export const __wbg_wingmemoryengine_free: (a: number, b: number) => void;
+export const wingmemoryengine_new: () => number;
+export const wingmemoryengine_load_model: (a: number, b: number, c: number, d: number, e: number) => [number, number];
+export const wingmemoryengine_load_tokenizer: (a: number, b: number, c: number) => [number, number];
+export const wingmemoryengine_ingest: (a: number, b: number, c: number) => [number, number, number, number];
+export const wingmemoryengine_query: (a: number, b: number, c: number, d: number) => [number, number, number];
+export const wingmemoryengine_delete_chunk: (a: number, b: number) => void;
+export const wingmemoryengine_clear: (a: number) => void;
+export const wingmemoryengine_serialize: (a: number) => any;
+export const wingmemoryengine_load: (a: number, b: any) => [number, number];
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
